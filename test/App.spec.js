b80cfffd41f8d3cfa53483224c7b2495a1fe137e
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-09-19 15:59:50 -0400

    test preparation

--- test/App.spec.js
+++ test/App.spec.js
@@ -2,29 +2,4 @@
 
 // const { expect } = require('chai')
 // const React = require('react')
-// const Search = require('../js/Search')
-// const ShowCard = require('../js/ShowCard')
 // const { shallow, mount } = require('enzyme')
-// const { shows } = require('../public/data')
-//
-// describe('<Search />', () => {
-//   it('should render the brand', () => {
-//     // shallow - will render parent component but not its childrens( much faster! )
-//     const wrapper = shallow(<Search />)
-//     expect(wrapper.contains(<h1 className="brand">svideo</h1>)).to.be.true
-//   })
-//
-//   it('it should render as many tests as there are data for', () => {
-//     const wrapper = shallow(<Search />)
-//     expect(wrapper.find(ShowCard).length).to.equal(shows.length)
-//   })
-//   // use mount if you can't use shallow. Mount is slower!
-//   it('it should fiter correctly given new state', () => {
-//     const wrapper = mount(<Search />)
-//     const input = wrapper.find('.search-input')
-//     input.node.value = 'house'
-//     input.simulate('change')
-//     expect(wrapper.state('searchTerm')).to.equal('house')
-//     expect(wrapper.find('.show-card').length).to.equal(2)
-//   })
-// })
